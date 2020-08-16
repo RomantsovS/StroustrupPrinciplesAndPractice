5b2a258357ad10ce5583234eacc4bb1979071b26
RomantsovS <romancov123@gmail.com>
2020-08-16 18:28:09 +0300

    05_errors

---
+++ Part_1/05_Errors/drill/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cmath>
+#include <string>
+
+using std::cin;
+using std::cout;
+using std::endl;
+using std::string;
+
+int main() {
+    throw "test throw";
+
+    return 0;
+}
