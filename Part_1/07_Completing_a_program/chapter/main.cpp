9e6799dad0c2dd8f493546f065acfdff468376d8
RomantsovS <romancov123@gmail.com>
2020-09-24 22:45:30 +0300

    chapter 7 drill started

--- Part_1/07_Completing_a_program/chapter/main.cpp
+++ Part_1/07_Completing_a_program/chapter/main.cpp
@@ -2,12 +2,13 @@
 #include <cmath>
 #include <string>
 #include <exception>
+#include <vector>
 
+using std::cerr;
 using std::cin;
 using std::cout;
 using std::endl;
 using std::string;
-using std::cerr;
 
 const char number = '8';
 const char quit = 'q';
@@ -15,31 +16,73 @@ const char print = ';';
 const char prompt = '>';
 const char result = '=';
 
-class Token {
+const string declkey = "let";
+const char name = 'a';
+const char let = 'L';
+
+class Variable
+{
+public:
+	Variable(const string &str, double val) : name(str), value(val) {}
+	string name;
+	double value;
+};
+
+std::vector<Variable> var_table;
+
+double get_value(string s)
+// return the value of the Variable named s
+{
+	for (const Variable &v : var_table)
+		if (v.name == s)
+			return v.value;
+	throw std::invalid_argument("get: undefined variable " + s);
+}
+
+void set_value(string s, double d)
+// set the Variable named s to d
+{
+	for (Variable &v : var_table)
+		if (v.name == s)
+		{
+			v.value = d;
+			return;
+		}
+	throw std::invalid_argument("set: undefined variable" + s);
+}
+
+class Token
+{
 public:
 	char kind;
 	double value;
+	string name;
 
 	Token() = default;
-	Token(char ch)    // make a Token from a char
-        :kind(ch), value(0) { }
-    Token(char ch, double val)     // make a Token from a char and a double
-        :kind(ch), value(val) { }
+	Token(char ch) // make a Token from a char
+		: kind(ch), value(0)
+	{
+	}
+	Token(char ch, double val) : kind{ch}, value{val} {} // initialize kind and value
+	Token(char ch, string n) : kind{ch}, name{n} {}		 // initialize kind and name
 };
 
-class Token_stream {
+class Token_stream
+{
 public:
 	Token get();
 	// get a Token (get() is defined in §6.8.2)
 	void putback(Token t); // put a Token back
 	void ignore(char c);
+
 private:
-	bool full {false};
+	bool full{false};
 	// is there a Token in the buffer?
 	Token buffer; // here is where we keep a Token put back using putback()
 };
 
-void Token_stream::putback(Token t) {
+void Token_stream::putback(Token t)
+{
 	buffer = t;
 	// copy t to buffer
 	full = true;
@@ -48,7 +91,8 @@ void Token_stream::putback(Token t) {
 
 Token Token_stream::get()
 {
-	if (full) {
+	if (full)
+	{
 		// do we already have a Token ready?
 		full = false;
 		// remove Token from buffer
@@ -57,7 +101,8 @@ Token Token_stream::get()
 	char ch;
 	cin >> ch;
 	// note that >> skips whitespace (space, newline, tab, etc.)
-	switch (ch) {
+	switch (ch)
+	{
 	case print:
 		// for “print”
 	case quit:
@@ -68,25 +113,45 @@ Token Token_stream::get()
 	case '-':
 	case '*':
 	case '/':
-	case '{' :
-	case '}' :
-	case '!' :
+	case '{':
+	case '}':
+	case '!':
 	case '%':
+	case '=':
 		return Token{ch};
 	// let each character represent itself
 	case '.':
-	case '0': case '1': case '2': case '3': case '4':
-	case '5': case '6': case '7': case '8': case '9':
+	case '0':
+	case '1':
+	case '2':
+	case '3':
+	case '4':
+	case '5':
+	case '6':
+	case '7':
+	case '8':
+	case '9':
 	{
 		cin.putback(ch);
 		// put digit back into the input stream
 		double val;
 		cin >> val;
 		// read a floating-point number
-		return Token{number,val};
+		return Token{number, val};
 		// let ‘8’ represent “a number”
 	}
 	default:
+		if (isalpha(ch))
+		{
+			string s;
+			s += ch;
+			while (cin.get(ch) && (isalpha(ch) || isdigit(ch)))
+				s += ch;
+			cin.putback(ch);
+			if (s == declkey)
+				return Token{let}; // declaration keyword
+			return Token{name, s};
+		}
 		throw std::invalid_argument("Bad token");
 	}
 }
@@ -94,22 +159,25 @@ Token Token_stream::get()
 void Token_stream::ignore(char c) // c represents the kind of Token
 {
 	// first look in buffer:
-	if (full && c==buffer.kind) {
+	if (full && c == buffer.kind)
+	{
 		full = false;
 		return;
 	}
 	full = false;
 	// now search input:
 	char ch = 0;
-	while (cin>>ch)
-		if (ch==c) return;
+	while (cin >> ch)
+		if (ch == c)
+			return;
 }
 
-Token_stream ts; // provides get() and putback()
+Token_stream ts;	 // provides get() and putback()
 double expression(); // declaration so that primary() can call expression()
 
-int factorial(int x) {
-	if(x == 0)
+int factorial(int x)
+{
+	if (x == 0)
 		return 1;
 	return factorial(x - 1) * x;
 }
@@ -117,38 +185,45 @@ int factorial(int x) {
 double primary()
 {
 	Token t = ts.get();
-	switch (t.kind) {
+	switch (t.kind)
+	{
 	case '(': // handle ‘(‘ expression ‘)’
 	{
 		double d = expression();
 		t = ts.get();
-		if (t.kind != ')') throw std::invalid_argument("')' expected");
+		if (t.kind != ')')
+			throw std::invalid_argument("')' expected");
 		return d;
 	}
 	case '{': // handle ‘{‘ expression ‘}’
 	{
 		double d = expression();
 		t = ts.get();
-		if (t.kind != '}') throw std::invalid_argument("'}' expected");
+		if (t.kind != '}')
+			throw std::invalid_argument("'}' expected");
 		return d;
 	}
-	case number: {
+	case number:
+	{
 		Token tok = ts.get();
-		if(tok.kind == '!')
+		if (tok.kind == '!')
 			return factorial(t.value);
-		else {
+		else
+		{
 			ts.putback(tok);
-		// we use ‘8’ to represent a number
+			// we use ‘8’ to represent a number
 			return t.value;
 		}
 		// return the number’s value
 	}
+	case name:
+		return get_value(t.name);
 	case '-':
 		return -primary();
 	case '+':
 		return primary();
 	default:
-		auto str = string("primary expected").append(1, t.kind);
+		auto str = string("primary expected: ").append(1, t.kind);
 		throw(std::invalid_argument(str));
 	}
 }
@@ -157,8 +232,10 @@ double term()
 {
 	double left = primary();
 	Token t = ts.get();
-	while (true) {
-		switch (t.kind) {
+	while (true)
+	{
+		switch (t.kind)
+		{
 		case '*':
 			left *= primary();
 			t = ts.get(); // get the next Token from the Token streamt = ts.get();
@@ -166,7 +243,8 @@ double term()
 		case '/':
 		{
 			double d = primary();
-			if (d == 0) throw(std::invalid_argument("divide by zero"));
+			if (d == 0)
+				throw(std::invalid_argument("divide by zero"));
 			left /= d;
 			t = ts.get();
 			break;
@@ -174,11 +252,13 @@ double term()
 		case '!':
 			return factorial(left);
 			break;
-		case '%': {
+		case '%':
+		{
 			int i1 = static_cast<int>(left);
 			int i2 = static_cast<int>(primary());
-			if (i2 == 0) throw("%: divide by zero");
-			left = i1%i2;
+			if (i2 == 0)
+				throw("%: divide by zero");
+			left = i1 % i2;
 			t = ts.get();
 			break;
 		}
@@ -190,11 +270,14 @@ double term()
 	}
 }
 
-double expression() {
+double expression()
+{
 	double left = term();
 	Token t = ts.get();
-	while (true) {
-		switch (t.kind) {
+	while (true)
+	{
+		switch (t.kind)
+		{
 		case '+':
 			left += term();
 			t = ts.get();
@@ -205,15 +288,63 @@ double expression() {
 			break;
 		default:
 			ts.putback(t);
-		return left;
+			return left;
 		}
 	}
-// read and evaluate a Term
-// get the next Token from the Token stream
-// evaluate Term and add
-// evaluate Term and subtract
-// put t back into the token stream
-// finally: no more + or –; return the answer
+	// read and evaluate a Term
+	// get the next Token from the Token stream
+	// evaluate Term and add
+	// evaluate Term and subtract
+	// put t back into the token stream
+	// finally: no more + or –; return the answer
+}
+
+bool is_declared(string var)
+// is var already in var_table?
+{
+	for (const Variable &v : var_table)
+		if (v.name == var)
+			return true;
+	return false;
+}
+
+double define_name(string var, double val)
+// add (var,val) to var_table
+{
+	if (is_declared(var))
+		throw std::invalid_argument(var + " declared twice");
+	var_table.push_back(Variable(var, val));
+	return val;
+}
+
+double declaration()
+// assume we have seen "let”
+// handle: name = expression
+// declare a variable called "name” with the initial value "expression”
+{
+	Token t = ts.get();
+	if (t.kind != name)
+		throw std::invalid_argument("name expected in declaration");
+	string var_name = t.name;
+	Token t2 = ts.get();
+	if (t2.kind != '=')
+		throw std::invalid_argument("= missing in declaration of " + var_name);
+	double d = expression();
+	define_name(var_name, d);
+	return d;
+}
+
+double statement()
+{
+	Token t = ts.get();
+	switch (t.kind)
+	{
+	case let:
+		return declaration();
+	default:
+		ts.putback(t);
+		return expression();
+	}
 }
 
 void clean_up_mess()
@@ -222,36 +353,50 @@ void clean_up_mess()
 	ts.ignore(print);
 }
 
-void calculate() {
-	while (cin) 
-	try {
-		cout << prompt;
-		Token t = ts.get();
-		// ‘q’ for “quit”
-		while (t.kind == print) t = ts.get();
-		if (t.kind == quit) break;
-		ts.putback(t);
-		cout << result << expression() << endl;
-	}
-	catch(std::exception& e) {
-		cerr << e.what() << endl;
-		clean_up_mess();
-	}
+void calculate()
+{
+	while (cin)
+		try
+		{
+			cout << prompt;
+			Token t = ts.get();
+			// ‘q’ for “quit”
+			while (t.kind == print)
+				t = ts.get();
+			if (t.kind == quit)
+				break;
+			ts.putback(t);
+			cout << result << statement() << endl;
+		}
+		catch (std::exception &e)
+		{
+			cerr << e.what() << endl;
+			clean_up_mess();
+		}
 }
 
-int main() {
-    try {
+int main()
+{
+	try
+	{
+		// predefine names:
+		define_name("pi", 3.1415926535);
+		define_name("e", 2.7182818284);
+
 		calculate();
 	}
-	catch (std::exception& e) {
+	catch (std::exception &e)
+	{
 		cerr << e.what() << '\n';
 		// keep_window_open():
 		cout << "Please enter the character ~ to close the window\n";
-		for (char ch; cin >> ch; )
-		// keep reading until we find a ~
-		if (ch=='~') return 1;
+		for (char ch; cin >> ch;)
+			// keep reading until we find a ~
+			if (ch == '~')
+				return 1;
 	}
-	catch (...) {
+	catch (...)
+	{
 		cerr << "exception \n";
 
 		return 2;
