b2ad1886bb18ed14f00a8dec58f0b3a2f6733a42
RomantsovS <romancov123@gmail.com>
2021-01-31 16:53:32 +0300

    added ch 12 drill

--- Part_2/12_A_display_model/drill/main.cpp
+++ Part_2/12_A_display_model/drill/main.cpp
@@ -4,16 +4,17 @@
 // "Programming -- Principles and Practice Using C++" by Bjarne Stroustrup
 //
 
-//#include "../../../GUI/Simple_window.h"    // get access to our window library
-//#include "../../../GUI/Graph.h"            // get access to our graphics library facilities
+#include "../../../GUI/Simple_window.h"    // get access to our window library
+#include "../../../GUI/Graph.h"            // get access to our graphics library facilities
 
 //------------------------------------------------------------------------------
 
-int main()
-{
-    /*using namespace Graph_lib;   // our graphics facilities are in Graph_lib
+int main() {
+    using namespace Graph_lib;   // our graphics facilities are in Graph_lib
 
     Point tl(100,100);           // to become top left  corner of window
+
+    std::cout << tl.x << std::endl;
 
     Simple_window win(tl,600,400,"Canvas");    // make a simple window
 
@@ -24,11 +25,30 @@ int main()
     poly.add(Point(400,200));    // add a third point 
 
     poly.set_color(Color::red);  // adjust properties of poly
-
+    poly.set_style(Line_style(Line_style::dash, 4));
     win.attach (poly);           // connect poly to the window
 
+    Axis xa{Axis::x, Point{20, 300}, 280, 10, "x axis"};
+    win.attach(xa);
+    Axis ya{Axis::y, Point{20, 300}, 280, 10, "y axis"};
+    ya.set_color(Color::red);
+    win.attach(ya);
+
+    Function sine{sin, 0, 100, Point{20, 150}, 1000, 50, 50};
+    win.attach(sine);
+
+    Rectangle r{Point{200, 200}, 100, 50};
+    r.set_fill_color(Color::yellow);
+    win.attach(r);
+
+    Text t{Point{150, 150}, "Hello"};
+    win.attach(t);
+
+    Image ii{Point{10, 10}, "image.jpg"};
+    win.attach(ii);
+
     win.wait_for_button();       // give control to the display engine
-    */
+
 }
 
 //------------------------------------------------------------------------------
